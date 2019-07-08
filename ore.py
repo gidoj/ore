d0eddc2823b8115272b90e5cdd7cbd7f822d7ca3
Joseph Gido <jgido1@verizon.net>
2019-07-08 04:08:19 +0100

    initialize ore.py file; add some test code

---
+++ ore.py
@@ -0,0 +1,14 @@
+import readline as rdl
+import inspect
+
+class Ore(object):
+
+    def __init__(self):
+        print("Ore started.")
+
+
+    def test(self):
+        ('Test docs:\n'
+         '-Some more text.\n')
+        t = "test variable"
+        print('test method')
